17813f09e3e0a02af25443e10bec5af37f3c9599
Daniel Alvarez <dnielalvrz@gmail.com>
2019-03-17 01:23:28 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,8 +9,8 @@ WIN_COMBINATIONS = [
                   ]
 
 def won?(board)
-  WIN_COMBINATIONS.detect do |wincomb|
-    board[wincomb[0]] == board[wincomb[1]] && board[wincomb[1]] == board[wincomb[2]] &&
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]] &&
     position_taken?(board, combo[0])
   end
 end
