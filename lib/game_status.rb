aa20cc9e086efd9a902e0a15dd7171fd2e58ff16
Daniel Alvarez <dnielalvrz@gmail.com>
2019-03-17 01:13:35 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,5 +7,14 @@ end
 WIN_COMBINATIONS = [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
 
 def won?(board)
-
+  for each win_combo in WIN_COMBINATIONS
+    win_index_0 = win_combination[0] 
+    win_index_1 = win_combination[1] 
+    win_index_2 = win_combination[2] 
+    win_index_3 = win_combination[3]
+    win_index_4 = win_combination[4]
+    win_index_5 = win_combination[5]
+    win_index_6 = win_combination[6]
+    win_index_7 = win_combination[7]
+    win_index_8 = win_combination[8]
 end
